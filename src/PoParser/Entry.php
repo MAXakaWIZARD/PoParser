c5d95a9602871931b0cc6c864e784982c7319034
MAXakaWIZARD <maxakawizard@gmail.com>
2015-04-10 00:23:16 +0300

    Add docblocks

--- src/PoParser/Entry.php
+++ src/PoParser/Entry.php
@@ -4,16 +4,34 @@ namespace PoParser;
 
 class Entry
 {
+    /**
+     * @var string
+     */
     protected $msgId;
 
+    /**
+     * @var null
+     */
     protected $msgIdPlural;
 
+    /**
+     * @var bool
+     */
     protected $fuzzy = false;
 
+    /**
+     * @var bool
+     */
     protected $obsolete = false;
 
+    /**
+     * @var array
+     */
     protected $translations = array();
 
+    /**
+     * @param $properties
+     */
     public function __construct($properties)
     {
         $this->msgId = $properties['msgid'];
@@ -23,36 +41,59 @@ class Entry
         $this->translations = $properties['msgstr'];
     }
 
+    /**
+     * @return bool
+     */
     public function isFuzzy()
     {
         return $this->fuzzy;
     }
 
+    /**
+     * @return string
+     */
     public function getMsgId()
     {
         return is_array($this->msgId) ? implode('', $this->msgId) : $this->msgId;
     }
 
+    /**
+     * @return null|string
+     */
     public function getMsgIdPlural()
     {
         return is_array($this->msgIdPlural) ? implode('', $this->msgIdPlural) : $this->msgIdPlural;
     }
 
+    /**
+     * @return bool
+     */
     public function isObsolete()
     {
         return $this->obsolete;
     }
 
+    /**
+     * @return array
+     */
     public function getTranslations()
     {
         return $this->translations;
     }
 
+    /**
+     * @param $index
+     *
+     * @return string
+     */
     public function getTranslation($index)
     {
         return (isset($this->translations[$index])) ? $this->translations[$index] : '';
     }
 
+    /**
+     * @return bool
+     */
     public function isPlural()
     {
         return !empty($this->msgIdPlural);
