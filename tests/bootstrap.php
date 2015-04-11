5da1c84a8399ee7f3765cabf1daad283a1072d71
MAXakaWIZARD <maxakawizard@gmail.com>
2015-04-11 16:23:00 +0300

    Fix BASE_PATH constant value

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
 
-define('BASE_PATH', realpath(dirname(__FILE__)) . '/..');
+define('BASE_PATH', realpath(dirname(__FILE__) . '/..'));
 
 require_once(BASE_PATH . '/vendor/autoload.php');
